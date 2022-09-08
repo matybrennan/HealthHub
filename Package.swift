8f6061963cb30f76e6f05d9b91841f71d20716e9
matybrennan <matybrennan@hipagesgroup.com.au>
2022-09-08 12:43:37 +1000

    release/2.0.0: Update package version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MBHealthTracker",
-    platforms: [.iOS("15.0")],
+    platforms: [.iOS("15.4")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
